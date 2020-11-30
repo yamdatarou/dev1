e84ff69a37280c259fe6c7c7530c35fa38861828
yamdatarou <tomo1tomo14@gmail.com>
2020-11-30 23:46:08 +0900

    Update openAll.js

--- src/Js/openAll.js
+++ src/Js/openAll.js
@@ -1,3 +1,7 @@
+/**
+ * ブラウザのお気に入りに追加したをURLに貼り付け
+ * javascript: var targetList = document.querySelector('#files').children.item(0),%20%20%20%20%20openClass1%20=%20'open',%20%20%20%20%20openClass2%20=%20'Details--on';%20%20%20%20%20console.log('----');%20targetList.childNodes.forEach(function(val){%20%20%20if%20(val.classList)%20{%20%20%20val.classList.toggle(openClass1);%20%20%20%20%20val.classList.toggle(openClass2);%20%20%20}%20});
+ */
 console.log('GitHubのPR　File changed 一括操作');
 var targetList = document.querySelector('#files').children.item(0),
     openClass1 = 'open',
