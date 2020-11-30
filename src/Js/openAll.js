61e0e1ffece49068b43b167e720b66eb8e7aa362
yamdatarou <tomo1tomo14@gmail.com>
2020-11-30 23:34:16 +0900

    Update openAll.js

--- src/Js/openAll.js
+++ src/Js/openAll.js
@@ -1,2 +1,11 @@
-// github のPRでソースを開く
-var target = document.querySelector('');
+console.log('GitHubのPR　File changed 一括操作');
+var targetList = document.querySelector('#files').children.item(0),
+    openClass1 = 'open',
+    openClass2 = 'Details--on';
+    console.log('----');
+targetList.childNodes.forEach(function(val){
+  if (val.classList) {
+  	val.classList.toggle(openClass1);
+    val.classList.toggle(openClass2);
+  }
+});
